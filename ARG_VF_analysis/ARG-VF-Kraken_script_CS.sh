3f1a25d8c90f3e8116413fe7fb0d5d34bde5ac5b
carolinescranton01 <carolinescranton@arizona.edu>
2025-05-07 13:43:29 -0700

    Bash script upload

---
+++ ARG_VF_analysis/ARG-VF-Kraken_script_CS.sh
@@ -0,0 +1,55 @@
+
+mkdir fastq_all
+mkdir KRAKEN2
+mkdir DeepARG
+mkdir VFDB
+mkdir NCBI
+mkdir CARD
+mkdir resfinder
+
+cd raw_reads
+for d in */
+do
+cd "$d"
+gunzip *.gz
+cat *.fastq > "$(pwd).fastq"
+cp "$(pwd).fastq" ../../fastq_all
+cd ..
+done
+
+conda activate Antibiotics
+
+cd ../fastq_all
+for f in *.fastq
+do
+n=${f%%.fastq}
+abricate --db vfdb --threads 94 --mincov 15 ${n}.fastq > ${n}_VFDB.tab
+abricate --db resfinder --threads 94 --mincov 15 ${n}.fastq > ${n}_resfinder.tab
+abricate --db ncbi --threads 94 --mincov 15 ${n}.fastq > ${n}_NCBI.tab
+abricate --db card --threads 94 --mincov 15 ${n}.fastq > ${n}_CARD.tab
+cp *CARD.tab ../CARD
+cp *NCBI.tab ../NCBI
+cp *VFDB.tab ../VFDB
+cp *resfinder.tab ../resfinder
+done
+
+for f in *.fastq
+do
+n=${f%%.fastq}
+deeparg predict --model SS --type nucl --input ${n}.fastq --out ${n}_deeparg -d /xdisk/kcooper/carolinescranton/deeparg_database
+cp *_deeparg.mapping.ARG ../DeepARG
+done
+
+conda deactivate
+conda activate Metagenomics
+for f in *.fastq
+do
+n=${f%%.fastq}
+kraken2 --db /xdisk/kcooper/kcooper/Kraken_Special_DB --report ${n}_K2_report.txt --output ${n}_K2_output.txt ${n}.fastq
+cp *.txt ../KRAKEN2
+done
+
+# make sure that kraken.biom file is in KRAKEN folder, titled 'metadata.tsv'
+
+kraken2-biom *_report.txt -m metadata.tsv -o output.biom
+
